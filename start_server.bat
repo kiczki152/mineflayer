41f8dafc6a36556b959ac7a93b735d656d1f8d93
kiczki152 <kiczki152@gmail.com>
2024-12-31 10:30:39 +0100

    My testing setup

---
+++ start_server.bat
@@ -0,0 +1,5 @@
+# download file https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
+# download file https://download.visualstudio.microsoft.com/download/pr/3ba1c632-ab22-49a0-affd-9a054f566ab1/b941979622251f2c11432471d23162eb/microsoft-jdk-21.0.5-windows-x64.msi
+# download file https://nodejs.org/dist/v22.12.0/node-v22.12.0-x64.msi
+# download file https://www.minecraft.net/bedrockdedicatedserver/bin-win/bedrock-server-1.21.51.02.zip
+java -Xmx1024M -Xms1024M -jar server.jar
